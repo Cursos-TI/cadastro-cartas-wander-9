d7806fc78974b2427d81a0e85d7abb6f0622f1fd
wander-9 <wandersonmilucaua@gmail.com>
2025-11-22 14:24:02 +0000

    cadastro das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,72 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado1;
+    char codigo1[20], cidade1[30];
+    float area1, pib1;
+    int populacao1, pontosTuristicos1;
+
+    char estado2;
+    char codigo2[20], cidade2[30];
+    float area2, pib2;
+    int populacao2, pontosTuristicos2;
 
   // Área para entrada de dados
+    printf("VAMOS CADASTRAR A SUA PRIMEIRA CARTA\n\n");
+
+    printf("Digite a letra do estado da sua carta:\n");
+    scanf("%c",&estado1);
+
+    printf("Digite a letra do seu estado seguido de um numero do 01 ao 04:\n");
+    scanf("%s", codigo1);
+
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", cidade1);
+
+    printf("Digite o número de habitantes da cidade:\n");
+    scanf("%d",&populacao1);
+
+    printf("Digite a área em quilometros quadrados:\n");
+    scanf("%f",&area1);
+    
+    printf("Digite o Produto Interno Bruto da cidade:\n");
+    scanf("%f",&pib1);
+
+    printf("Digite o número de pontos turistos da cidade:\n");
+    scanf("%d",&pontosTuristicos1);
+
+
+    printf("\nAGORA VAMOS CADASTRAR A SUA SEGUNDA CARTA\n\n");
+
+    printf("Digite a letra do estado da sua carta:\n");
+    scanf(" %c",&estado2);
+
+    printf("Digite a letra do seu estado seguido de um numero do 01 ao 04:\n");
+    scanf("%s", codigo2);
+
+    printf("Digite o nome da cidade:\n");
+    scanf("%s", cidade2);
+
+    printf("Digite o número de habitantes da cidade:\n");
+    scanf("%d",&populacao2);
+
+    printf("Digite a área em quilometros quadrados:\n");
+    scanf("%f",&area2);
+    
+    printf("Digite o Produto Interno Bruto da cidade:\n");
+    scanf("%f",&pib2);
+
+    printf("Digite o número de pontos turistos da cidade:\n");
+    scanf("%d",&pontosTuristicos2);
 
   // Área para exibição dos dados da cidade
+    printf("\nCARTAS CADASTRADAS COM SUCESSO!\n\n");
+
+    printf("---CARTA 1---\n\nESTADO:%c\nCÓGIDO:%s\nNOME DA CIDADE:%s\nPOPULAÇÃO:%d\nÁREA:%.2f km2\nPIB:%.2f R$\nNÚMERO DE PONTOS TURÍSTICOS:%d\n\n", estado1, codigo1, cidade1, populacao1, area1, pib1, pontosTuristicos1);
+
+    printf("---CARTA 2---\n\nESTADO:%c\nCÓGIDO:%s\nNOME DA CIDADE:%s\nPOPULAÇÃO:%d\nÁREA:%.2f km2\nPIB:%.2f R$\nNÚMERO DE PONTOS TURÍSTICOS:%d\n\n", estado2, codigo2, cidade2, populacao2, area2, pib2, pontosTuristicos2);
+
+    return 0;
 
 return 0;
 } 
